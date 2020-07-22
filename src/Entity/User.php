6e9299a48e5ccb3a0fd2d1f4af80b7c10acc35a5
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-07-22 17:59:15 +0200

    Provide default timestamps in User constructor

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -58,7 +58,9 @@ class User implements UserInterface
          * the database. This provides a default and allows to only care about
          * changing updateAt on edits.
          */
-        $this->createdAt = new \DateTime();
+        $now = new \DateTime();
+        $this->createdAt = $now;
+        $this->updatedAt = $now;
     }
 
     public function getId(): ?int
