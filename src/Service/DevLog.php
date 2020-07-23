b034aca7a31e56bb013fa50c83fcfcc72caf2cef
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-07-23 22:24:18 +0200

    Make note optional and logging directly from constructor in DevLog

--- src/Service/DevLog.php
+++ src/Service/DevLog.php
@@ -2,28 +2,11 @@
 
 namespace App\Service;
 
-// use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 
 class DevLog
 {
-    // private static $instance = null;
-
-    // private function __construct()
-    // {
-    // }
-
-    // public static function get()
-    // {
-    //     if(self::$instance === null)
-    //     {
-    //         self::$instance = new DevLog();
-    //     }
-
-    //     return self::$instance;
-    // }
     /**
      * @var array <string, mixed>[] [$field_name => $value]
      */
@@ -31,9 +14,8 @@ class DevLog
     private static $cloner;
     private static $dumper;
 
-    public function __construct() //(RouterInterface $router) //(string $name, $variable) //, VarCloner $cloner)
+    public function __construct($variable = null, ?string $note = null)
     {
-        // self::$data = array_merge(self::$data, $data);
         if (self::$cloner === null) {
             self::$cloner = new VarCloner();
         }
@@ -41,15 +23,9 @@ class DevLog
             self::$dumper = new HtmlDumper();
         }
 
-        // if(!isset(self::$data)){
-        //     self::$data[] = '';
-        // }
-        // if (self::$router === null) {
-        //     self::$router = $router;
-        //     // dd(self::$router->getRouteCollection()->all());
-        // }
-        // self::$data[$name] = self::$cloner->cloneVar($variable);
-        // self::$data[$name] = $variable;
+        if($variable !== null) {
+            $this->log($variable, $note);
+        }
     }
 
     /**
@@ -60,47 +36,23 @@ class DevLog
         return self::$data[$property];
     }
 
-    public function log(string $name, $variable)
+    public function log($variable, ?string $note = null) :self
     {
-        // if (self::$cloner === null) {
-        //     self::$cloner = new VarCloner();
-        // }
-        // self::$data[$name] = $variable;
-        // self::$data[$name] = self::$cloner->cloneVar($variable);
-        self::$data[$name] = self::$dumper->dump(
+        $dump = self::$dumper->dump(
             self::$cloner->cloneVar($variable), true
         );
-        // dump(self::$data);
+        
+        if($note === null ){
+            self::$data[] = $dump;
+         }else {
+            self::$data[$note] = $dump;
+        }
+
+        return $this;
     }
 
     public function getData()
     {
         return self::$data;
     }
-    // public function getRoutes()
-    // {
-    //     /**
-    //      * Do NOT use getRouteCollection() in prod, it regenerates Routing cache
-    //      * on each request.
-    //      */
-    //     return self::$router->getRouteCollection()->all();
-    // }
-
-    // public function getParameterLessRoutes(): array
-    // {
-    //     /**
-    //      * Do NOT use getRouteCollection() in prod, it regenerates Routing cache
-    //      * on each request.
-    //      */
-    //     $routes = self::$router->getRouteCollection()->all();
-
-
-    //     $parameterLessRoutes = [];
-    //     foreach ($routes as $name => $route) {
-    //         if (!strpos($route->getPath(), '{')) {
-    //             $parameterLessRoutes[] = $name;
-    //         }
-    //     }
-    //     return $parameterLessRoutes;
-    // }
 }
